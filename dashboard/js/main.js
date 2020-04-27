c8a19df57a6142fadf72709a20e8f5c41e9bbb69
Allarassem maxwell <allarassemmaxwell@Allarassems-MacBook-Air.local>
2020-04-27 05:40:43 +0300

    Adding Jquery validation

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -1,5 +1,112 @@
 $(document).ready(function() {
+
+
 	$('.nav-trigger').click(function() {
 		$('.side-nav').toggleClass('visible');
 	});
+
+
+
+	// EXPENSE ADD VALIDATION
+	$(".add-expense-form").validate({
+		rules: {
+			category: 'required',
+			product_or_service: 'required',
+			price: {
+				required: true,
+				number: true
+			}
+		}
+		// submitHandler: function() { alert("Submitted!") }
+	});
+
+
+	
+	// SOURCE ADD VALIDATION
+	$(".add-source-form").validate({
+		rules: {
+			name: 'required'
+		}
+		// submitHandler: function() { alert("Submitted!") }
+	});
+
+
+
+	// INCOME ADD VALIDATION
+	$(".add-income-form").validate({
+		rules: {
+			source: 'required',
+			amount: {
+				required: true,
+				number: true
+			}
+		}
+		// submitHandler: function() { alert("Submitted!") }
+	});
+
+
+
+	// CATEGORY ADD VALIDATION
+	$(".add-category-form").validate({
+		rules: {
+			name: 'required'
+		}
+		// submitHandler: function() { alert("Submitted!") }
+	});
+
+
+
+	// INCOME ADD VALIDATION
+	$(".product-service-form").validate({
+		rules: {
+			category: 'required',
+			name: 'required'
+		}
+		// submitHandler: function() { alert("Submitted!") }
+	});
+
+
+
+
+	// PROFILE EDIT VALIDATION
+	$(".profile-edit-form").validate({
+		rules: {
+			first_name: 'required',
+			last_name: 'required',
+			email: { 
+				required: true,
+				email: true
+			},
+			family_number: { 
+				required: true,
+				number: true
+			}
+		}
+		// submitHandler: function() { alert("Submitted!") }
+	});
+
+
+
+
+	// CHANGE PASSWORD VALIDATION
+	$(".change-password-form").validate({
+		rules: {
+			old_password: 'required',
+			new_password: { 
+				required: true,
+				minlength: 6
+			},
+			c_password: { 
+				required: true,
+				equalTo : "#new_password"
+			}
+
+		}
+		// submitHandler: function() { alert("Submitted!") }
+	});
+
+
+
+
+
 });
--- js/main.js
+++ js/main.js
@@ -33,7 +33,55 @@ $(document).ready(function() {
     });
 
 
+    // LOGIN VALIDATION
+    $(".login-form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: 'required'
+        }
+    });
+
+    
+    // CREATE ACCOUNT VALIDATION
+    $(".register-form").validate({
+        rules: {
+            first_name: 'required',
+            last_name: 'required',
+            email: {
+                required: true,
+                email: true
+            },
+            password1: {
+                required: true,
+                minlength: 6
+            },
+            password2: {
+                required: true,
+                equalTo : "#password1"
+            }
+        }
+        // submitHandler: function() { alert("Submitted!") }
+   
+    });
+
+
+
+    // RESET PASSWORD VALIDATION
+    $(".reset-password-form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            }
+        }
+    });
+
+
 
     
 });
+
 
